1e12db9ba6d45569e41fd04a7391fb1a8f0e9c45
piekario13 <piekario13@gmail.com>
2017-10-26 11:28:11 +0200

    comment

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var name = prompt('Enter your name');
+//Komentarz na temat kodu
+alert('Hello, ' + name);
+//Komantarz na temat kodu
+console.log('Hello, ' + name);
+/* 
+Komentarz
+na temat
+kodu
+*/
